5a546180f4db6deb59b6c744ffb2547d3c117f87
Wolfgang Kreminger <r4pt0s@protonmail.com>
2024-09-25 17:24:40 +0200

    set camscale in main

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { k } from './kplayCtx';
 
 import './scenes/start';
 import './scenes/city';
+import { setCamScale } from './utils';
+
+
+k.onResize(() => {
+  setCamScale(k);
+});
 
 k.go('start');
