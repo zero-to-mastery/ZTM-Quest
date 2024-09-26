37f60a74beb33a52bd4bed39d2e9382c48f4a6af
Wolfgang Kreminger <r4pt0s@protonmail.com>
2024-09-26 18:05:47 +0200

    add entry to arcade map

--- src/interactions/map_city/player.interactions.js
+++ src/interactions/map_city/player.interactions.js
@@ -2,4 +2,7 @@ import { displayDialogue } from '../../utils';
 
 export const initPlayerInteractions = (player, k) => {
 
+  player.onCollide('enter_map_arcade', () => {
+    k.go('arcade');
+  });
 }
