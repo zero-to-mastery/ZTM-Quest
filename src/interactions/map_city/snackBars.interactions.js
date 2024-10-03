d3366630196f0ae5781aaaecdf00ff2a5bb0c42c
Kevin Batdorf <kbat82@gmail.com>
2024-10-04 03:15:43 +0700

    fix more text

--- src/interactions/map_city/snackBars.interactions.js
+++ src/interactions/map_city/snackBars.interactions.js
@@ -1,69 +1,84 @@
 import { displayPermissionBox } from '../../utils';
 
-
 export const snackBarGreenInteraction = (player, k) => {
     player.onCollide('snack_bar_green', async () => {
-        let ztmSnackDecision = await displayPermissionBox(
-            'Welcome to Snack Bar Green! Ready to go from zero to snack mastery?'
-        );
+        let ztmSnackDecision = await displayPermissionBox([
+            'Welcome to Snack Bar Green! Ready to go from zero to snack mastery?',
+        ]);
 
         if (ztmSnackDecision) {
-            k.debug.log('Congratulations! You’ve mastered the art of snacking, ZTM style!'); 
-        } else {
-            let secondSnackChance = await displayPermissionBox(
-                'Are you sure? The path to snack mastery is only one bite away...'
+            k.debug.log(
+                'Congratulations! You’ve mastered the art of snacking, ZTM style!'
             );
+        } else {
+            let secondSnackChance = await displayPermissionBox([
+                'Are you sure? The path to snack mastery is only one bite away...',
+            ]);
 
             if (secondSnackChance) {
-                k.debug.log('Great choice! You’re on your way to becoming a Snack Sensei!');
+                k.debug.log(
+                    'Great choice! You’re on your way to becoming a Snack Sensei!'
+                );
             } else {
-                k.debug.log('Looks like you missed the Snack Mastery Bootcamp. Better luck next time!'); 
+                k.debug.log(
+                    'Looks like you missed the Snack Mastery Bootcamp. Better luck next time!'
+                );
             }
         }
     });
 };
-
 
 export const snackBarRedInteraction = (player, k) => {
     player.onCollide('snack_bar_red', async () => {
-        let snackOffer = await displayPermissionBox(
-            'Welcome to Snack Bar Red! The *real* masters of snacking... Unlike *ahem* some green wannabes across the street. Ready for the best snack experience?'
-        );
+        let snackOffer = await displayPermissionBox([
+            'Welcome to Snack Bar Red! The *real* masters of snacking... Unlike *ahem* some green wannabes across the street. Ready for the best snack experience?',
+        ]);
 
         if (snackOffer) {
-            k.debug.log('Excellent choice! You clearly have superior taste. Snack Bar Green can’t compete!'); 
-        } else {
-            let changeMind = await displayPermissionBox(
-                'Are you sure? Don’t tell me you’re thinking of going to *that* green place... Their snacks are... well, let’s just say "unimpressive."'
+            k.debug.log(
+                'Excellent choice! You clearly have superior taste. Snack Bar Green can’t compete!'
             );
+        } else {
+            let changeMind = await displayPermissionBox([
+                'Are you sure? Don’t tell me you’re thinking of going to *that* green place... Their snacks are... well, let’s just say "unimpressive."',
+            ]);
 
             if (changeMind) {
-                k.debug.log('Wise move! Snack Bar Red – where the real snack mastery happens!'); 
+                k.debug.log(
+                    'Wise move! Snack Bar Red – where the real snack mastery happens!'
+                );
             } else {
-                k.debug.log('Suit yourself! But don’t say I didn’t warn you... Snack Bar Green’s snacks are basically "Zero to Mediocrity."'); 
+                k.debug.log(
+                    'Suit yourself! But don’t say I didn’t warn you... Snack Bar Green’s snacks are basically "Zero to Mediocrity."'
+                );
             }
         }
     });
 };
 
-
 export const snackBarStreetInteraction = (player, k) => {
     player.onCollide('snack_bar_street', async () => {
-        let snackDecision = await displayPermissionBox(
-            'Psst! You look hungry! How about a snack? It’s a secret recipe!'
-        );
+        let snackDecision = await displayPermissionBox([
+            'Psst! You look hungry! How about a snack? It’s a secret recipe!',
+        ]);
 
         if (snackDecision) {
-            k.debug.log('Yum! You just ate the best mystery snack of your life!'); 
-        } else {
-            let secondChance = await displayPermissionBox(
-                'Are you sure? It’s low calorie and totally NOT haunted! Still no?'
+            k.debug.log(
+                'Yum! You just ate the best mystery snack of your life!'
             );
+        } else {
+            let secondChance = await displayPermissionBox([
+                'Are you sure? It’s low calorie and totally NOT haunted! Still no?',
+            ]);
 
             if (secondChance) {
-                k.debug.log('Ah, there we go! Enjoy your... totally normal snack.'); 
+                k.debug.log(
+                    'Ah, there we go! Enjoy your... totally normal snack.'
+                );
             } else {
-                k.debug.log('Your loss! The snack of the century is only a bite away...'); 
+                k.debug.log(
+                    'Your loss! The snack of the century is only a bite away...'
+                );
             }
         }
     });
--- src/interactions/map_city/stalls.interaction.js
+++ src/interactions/map_city/stalls.interaction.js
@@ -1,23 +1,28 @@
 import { displayPermissionBox } from '../../utils';
 
-
 export const stall1Interaction = (player, k) => {
     player.onCollide('stall_1', async () => {
-        let stall1Offer = await displayPermissionBox(
-            '🎉 Welcome to Stall 1! Our secret ingredient is... a sprinkle of **joy**! Ready to snack like a champ?'
-        );
+        let stall1Offer = await displayPermissionBox([
+            '🎉 Welcome to Stall 1! Our secret ingredient is... a sprinkle of **joy**! Ready to snack like a champ?',
+        ]);
 
         if (stall1Offer) {
-            k.debug.log('🚀 Awesome choice! Here’s your snack – made with love and just a hint of magic!'); 
-        } else {
-            let secondChance = await displayPermissionBox(
-                '🤔 Are you absolutely sure? Our snacks are scientifically proven to elevate your happiness levels! Or do you prefer sadness-flavored snacks?'
+            k.debug.log(
+                '🚀 Awesome choice! Here’s your snack – made with love and just a hint of magic!'
             );
+        } else {
+            let secondChance = await displayPermissionBox([
+                '🤔 Are you absolutely sure? Our snacks are scientifically proven to elevate your happiness levels! Or do you prefer sadness-flavored snacks?',
+            ]);
 
             if (secondChance) {
-                k.debug.log('😋 Yass! You’re now part of the snack elite! Enjoy your crispy delight!'); 
+                k.debug.log(
+                    '😋 Yass! You’re now part of the snack elite! Enjoy your crispy delight!'
+                );
             } else {
-                k.debug.log('😢 Your loss! But hey, who am I to judge? Just remember, you’re missing out on epic flavor adventures!'); 
+                k.debug.log(
+                    '😢 Your loss! But hey, who am I to judge? Just remember, you’re missing out on epic flavor adventures!'
+                );
             }
         }
     });
@@ -25,21 +30,27 @@ export const stall1Interaction = (player, k) => {
 
 export const stall2Interaction = (player, k) => {
     player.onCollide('stall_2', async () => {
-        let stall2Offer = await displayPermissionBox(
-            '🎊 Ahoy! Welcome to Stall 2! We don’t just sell snacks; we sell **memories**! Care to join the flavor revolution?'
-        );
+        let stall2Offer = await displayPermissionBox([
+            '🎊 Ahoy! Welcome to Stall 2! We don’t just sell snacks; we sell **memories**! Care to join the flavor revolution?',
+        ]);
 
         if (stall2Offer) {
-            k.debug.log('🌈 Fantastic! Here’s a snack that might just change your life! Or at least your lunch!'); 
-        } else {
-            let secondThoughts = await displayPermissionBox(
-                '🚫 Wait a minute! Are you really about to miss the chance to munch on greatness? Don’t you want to be a snack legend?'
+            k.debug.log(
+                '🌈 Fantastic! Here’s a snack that might just change your life! Or at least your lunch!'
             );
+        } else {
+            let secondThoughts = await displayPermissionBox([
+                '🚫 Wait a minute! Are you really about to miss the chance to munch on greatness? Don’t you want to be a snack legend?',
+            ]);
 
             if (secondThoughts) {
-                k.debug.log('🌟 You’ve made the right call! Enjoy the epic flavors of Stall 2! Snack on, my friend!'); 
+                k.debug.log(
+                    '🌟 You’ve made the right call! Enjoy the epic flavors of Stall 2! Snack on, my friend!'
+                );
             } else {
-                k.debug.log('🤷‍♂️ Suit yourself! But don’t come crying to me when you realize Stall 1’s snacks are just plain!'); 
+                k.debug.log(
+                    '🤷‍♂️ Suit yourself! But don’t come crying to me when you realize Stall 1’s snacks are just plain!'
+                );
             }
         }
     });
