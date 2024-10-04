c435b6b1850b6283d2a87df82f9e3210925cb977
Ihliana <Ihnatco@protonmail.com>
2024-10-04 04:48:44 +0200

    npcInteractionHandler used

--- src/interactions/map_start/misterFu.interaction.js
+++ src/interactions/map_start/misterFu.interaction.js
@@ -1,4 +1,4 @@
-import {displayDialogueWithCharacter} from '../../utils'
+import {displayDialogueWithCharacter, npcInteractionHandler} from '../../utils'
 
 import { conversationMisterFu, misterFu } from '../../constants';
 
@@ -6,15 +6,17 @@ import { conversationMisterFu, misterFu } from '../../constants';
 export const interactionWithMisterFu =(player, k, map) => {
     player.onCollide('misterFu', () => {
         player.isInDialog = true;
-        displayDialogueWithCharacter({
-            k,
-            player,
-            characterName: misterFu.name,
-            test: conversationMisterFu,
-            onDisplayEnd: () => {
-                player.isInDialog = false;
-                player.state.hasTalkedToMisterFu = true;
-            },
-        });
+        npcInteractionHandler(player, misterFu.name, k, () => {
+            displayDialogueWithCharacter({
+                k,
+                player,
+                characterName: misterFu.name,
+                text: conversationMisterFu,
+                onDisplayEnd: () => {
+                    player.isInDialog = false;
+                    player.state.hasTalkedToMisterFu = true;
+                },
+            });
+        })
     });
 }
