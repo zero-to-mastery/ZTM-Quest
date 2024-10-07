bee66639902dfe51be8bc92c37ed2d91f229c573
purifiedlight <mikaeladam180@gmail.com>
2024-10-07 21:35:07 +0200

    test commit basic setup added

--- src/interactions/map_arcade/index.js
+++ src/interactions/map_arcade/index.js
@@ -1,6 +1,7 @@
 import { enterMapCityInteraction } from './enterMapCity.interactions';
 import { interactionWithGameMachine2 } from './game_machine_2.interactions';
 import { interactionWithGameMachine6 } from './game_machine_6.interactions';
+import { interactionWithGameMachine11 } from './game_machine_11.interaction';
 
 const interactions = [
     enterMapCityInteraction,
@@ -9,6 +10,7 @@ const interactions = [
     // new interaction
     interactionWithGameMachine2,
     interactionWithGameMachine6,
+    interactionWithGameMachine11,
 ];
 
 export const attachInteractions = (gameObj, k) => {
