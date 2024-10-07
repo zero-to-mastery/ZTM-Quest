7dd0ef632a390b562417a3da6b03a257e0f23418
Miro <lorenzo.miro.sd@gmail.com>
2024-10-07 12:44:20 -0700

    Fixing GameMachine interactions

--- src/interactions/map_arcade/index.js
+++ src/interactions/map_arcade/index.js
@@ -1,22 +1,13 @@
 import { storeMainAreaInteraction } from '../map_city/enterStoreMainArea.interaction';
 import { enterMapCityInteraction } from './enterMapCity.interactions';
-import { interactionWithGameMachine2 } from './game_machine_2.interactions';
-import { interactionWithGameMachine4 } from './game_machine_4.interactions';
-import { interactionWithGameMachine6 } from './game_machine_6.interactions';
 
 const interactions = [
     enterMapCityInteraction,
     // Add more interactions here
-<<<<<<< HEAD
     storeMainAreaInteraction,
-    interactionWithArcadeArea
-=======
-
-    // new interaction
+    interactionWithArcadeArea,
     interactionWithGameMachine2,
-    interactionWithGameMachine4,
-    interactionWithGameMachine6,
->>>>>>> 9e9f897d07f0657ac27665dadd59bf5fb272dc7f
+    interactionWithGameMachine6
 ];
 
 export const attachInteractions = (gameObj, k) => {
