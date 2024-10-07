5fcd2266df149c6532f57ae83f55939a7fa313d9
aadityaforwork <aadityamalani15@gmail.com>
2024-10-07 15:04:00 +0530

    game_machine_1 added

--- src/interactions/map_arcade/index.js
+++ src/interactions/map_arcade/index.js
@@ -1,12 +1,14 @@
 import { enterMapCityInteraction } from './enterMapCity.interactions';
+import { interactionWithGameMachine1 } from './game_machine_1.intercation';
 import { interactionWithGameMachine2 } from './game_machine_2.interactions';
 
 const interactions = [
     enterMapCityInteraction,
     // Add more interactions here
-
+    interactionWithGameMachine1,
     // new interaction
     interactionWithGameMachine2,
+    
 ];
 
 export const attachInteractions = (gameObj, k) => {
