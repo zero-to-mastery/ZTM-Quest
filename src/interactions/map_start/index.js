5bd18e5e6351ac8fbcbf68aaaae638162b8c8f2f
Miro <lorenzo.miro.sd@gmail.com>
2024-10-07 12:47:18 -0700

    Adding in map_start interactions

--- src/interactions/map_start/index.js
+++ src/interactions/map_start/index.js
@@ -1,40 +1,4 @@
 import { interactionWithBruno } from './bruno.interaction';
-import { enterMapCityLeftInteraction } from './enterMapCityLeft.interaction';
-import { enterMapCityRightInteraction } from './enterMapCityRight.interactions';
-import { interactionWithMainboxMainArea } from './mailboxMainArea.interaction';
-import { restroomInteractions } from './restroom.interactions';
-import { interactionWithComputer } from './computer.interaction';
-import { interactionWithJokeTeller } from './jokeTeller.interaction';
-import { interactionWithDrinksMachine } from './drink_machine.interaction';
-
-import { interactionWithMisterFu } from './misterFu.interaction';
-import { interactionWithTV } from './tv_main_room.interaction';
-import { interactionWithCake } from './cake.interaction';
-import { interactionWithLocker } from './locker.interaction';
-
-const interactions = [
-    restroomInteractions,
-    interactionWithBruno,
-    enterMapCityLeftInteraction,
-    enterMapCityRightInteraction,
-    interactionWithMainboxMainArea,
-    interactionWithComputer,
-    interactionWithJokeTeller,
-    interactionWithDrinksMachine,
-    // Add more interactions here
-    interactionWithMisterFu,
-    interactionWithTV,
-    interactionWithCake,
-    interactionWithLocker,
-];
-
-export const attachInteractions = (gameObj, k) => {
-    const map = k.get('main_map')[0];
-
-    interactions.forEach((cb) => cb(gameObj, k, map));
-};
-
-import { interactionWithBruno } from './bruno.interaction';
 import { enterMapCityInteraction } from './enterMapCity.interaction';
 import { interactionWithMainboxMainArea } from './mailboxMainArea.interaction';
 import { restroomInteractions } from './restroom.interactions';
@@ -45,3 +9,25 @@ import { interactionWithMisterFu } from './misterFu.interaction';
 import { interactionWithTV } from './tv_main_room.interaction';
 import { interactionWithCake } from './cake.interaction';
 import { interactionWithLocker } from './locker.interaction';
+
+const interactions = [
+    restroomInteractions,
+    interactionWithBruno,
+    enterMapCityInteraction,
+    interactionWithMainboxMainArea,
+    interactionWithComputer,
+    // Add more interactions here
+    interactionWithStoreMainArea,
+    interactionWithJokeTeller,
+    interactionWithDrinksMachine,
+    interactionWithMisterFu,
+    interactionWithTV,
+    interactionWithCake,
+    interactionWithLocker
+];
+
+export const attachInteractions = (gameObj, k) => {
+    const map = k.get('main_map')[0];
+
+    interactions.forEach((cb) => cb(gameObj, k, map));
+};
