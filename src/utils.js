34ef9a9f300aaaef2e2ecded6a9a64cd4a9ea667
Jacob Wilson <jcbwlsn04@gmail.com>
2024-10-07 08:56:45 -0500

    removed excess function not used

--- src/utils.js
+++ src/utils.js
@@ -145,33 +145,6 @@ export function setCamScale(k) {
     }
 }
 
-// NOTE: sprite must be an npc not an object like mailbox
-// export function buildActionModal(sprite, k) {
-//     document.getElementById("interaction-note").style.display = "flex";
-// const spritePos = sprite.pos;
-
-// const actionModal = k.add([
-//     k.sprite('action-modal'),
-//     k.area(),
-//     k.color(255, 255, 255),
-//     k.outline(2, k.Color.BLACK),
-//     k.pos(spritePos.x - 10, spritePos.y - sprite.height - 30),
-//     k.layer('ui'),
-//     `action-modal-${sprite.tags[0]}`,
-//     // k.offscreen({ hide: true, pause: true })
-// ]);
-
-// const actionLabel = k.add([
-//     k.text('T', { size: 16, align: 'center' }),
-//     k.color(0, 0, 0),
-//     k.pos(actionModal.pos.x + 5, actionModal.pos.y + 4),
-//     `action-label-${sprite.tags[0]}`,
-//     //k.offscreen({ hide: true, pause: true })
-// ]);
-
-// return { actionModal, actionLabel };
-// }
-
 export const buildInteractionPrompt = (sprite, k) => {
     if (k.isTouchscreen()) {
         document.getElementById('interaction-note-mobile').style.display =
