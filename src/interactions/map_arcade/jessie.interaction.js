ffff70dc56ddeffba015e4ecccd31c6730d611ee
Ihliana <Ihnatco@protonmail.com>
2024-10-08 04:57:29 +0200

    interactionHandler changed

--- src/interactions/map_arcade/jessie.interaction.js
+++ src/interactions/map_arcade/jessie.interaction.js
@@ -1,24 +1,19 @@
 import { displayDialogue } from '../../utils';
-
-import { npcInteractionHandler } from '../handler.interactions';
-
+import { interactionHandler } from '../handler.interactions';
 import { conversationJessie, jessie } from '../../constants';
 
 export const interactionWithJessie = (player, k, map) => {
-    player.onCollide('jessie', () => {
+    interactionHandler(player, jessie.name, k, () => {
         player.isInDialog = true;
-
-        npcInteractionHandler(player, jessie.name, k, () => {
-            displayDialogue({
-                k,
-                player,
-                characterName: jessie.name,
-                text: conversationJessie,
-                onDisplayEnd: () => {
-                    player.isInDialog = false;
-                    player.state.hasTalkedToJessie = true;
-                },
-            });
+        displayDialogue({
+            k,
+            player,
+            characterName: jessie.name,
+            text: conversationJessie,
+            onDisplayEnd: () => {
+                player.isInDialog = false;
+                player.state.hasTalkedToJessie = true;
+            },
         });
     });
 };
