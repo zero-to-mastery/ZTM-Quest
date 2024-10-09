d7c0969fa1405b29ad62969eea513d01226301ab
Ihliana <Ihnatco@protonmail.com>
2024-10-09 11:10:19 +0200

    displayDialogue

--- src/interactions/map_arcade/jessie.interaction.js
+++ src/interactions/map_arcade/jessie.interaction.js
@@ -1,5 +1,6 @@
-import { displayDialogue } from '../../utils';
+
 import { interactionHandler } from '../handler.interactions';
+import { displayDialogue } from '../../utils';
 import { conversationJessie, jessie } from '../../constants';
 
 export const interactionWithJessie = (player, k, map) => {
