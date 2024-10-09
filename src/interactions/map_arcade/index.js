2a94f2713f6d354060706e0efcda89ae26fda2b8
Ihliana <Ihnatco@protonmail.com>
2024-10-09 11:23:11 +0200

    interactionWithJessie

--- src/interactions/map_arcade/index.js
+++ src/interactions/map_arcade/index.js
@@ -1,22 +1,15 @@
 import { enterMapCityInteraction } from './enterMapCity.interactions';
-<<<<<<< HEAD
-
-import {interactionWithJessie} from './jessie.interaction';
-
-=======
 import { interactionWithGameMachine8 } from './game_machine_8';
->>>>>>> bda3a537a25f7769dd8bc32aa5975f9ed7303af0
 import { interactionWithGameMachine2 } from './game_machine_2.interactions';
 import { interactionWithGameMachine4 } from './game_machine_4.interactions';
 import { interactionWithGameMachine6 } from './game_machine_6.interactions';
-
+import { interactionWithJessie } from './jessie.interaction';
 
 const interactions = [
     enterMapCityInteraction,
     // Add more interactions here
 
     interactionWithJessie,
-
 
     // new interaction
     interactionWithGameMachine2,
