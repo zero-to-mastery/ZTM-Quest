f9888846a512f7a29de859392b1a11e52365e73c
poschuler <poschuler@gmail.com>
2024-10-09 20:46:35 -0500

    feat: add sleep beed interaction

--- src/interactions/map_campus_house_1/index.js
+++ src/interactions/map_campus_house_1/index.js
@@ -1,8 +1,10 @@
 import { enterMapCityInteraction } from './enterMapCity.interactions';
+import { bedInteractions } from './bed.interaction';
 
 const interactions = [
     // Add more interactions here
     enterMapCityInteraction,
+    bedInteractions,
 ];
 
 export const attachInteractions = (gameObj, k) => {
