3b7a8524bb33bf168d20f8892b2f232aed360890
kapish9741 <kapish9b3448@gmail.com>
2024-10-28 03:54:29 +0530

    add restroom_toilet interaction

--- src/interactions/map_campus_house_1/index.js
+++ src/interactions/map_campus_house_1/index.js
@@ -22,6 +22,7 @@ const interactions = [
     bedroomTableInteractions,
     livingroomcouchInteractions,
     bedroomPlantInteractions,
+    restroomToiletInteractions,
 ];
 
 export default interactions;
