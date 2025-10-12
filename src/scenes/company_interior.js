bee0df702bbcc69486abfa2ce1a15e35b19f89f9
leonyangela <38250310+leonyangela@users.noreply.github.com>
2025-10-12 16:49:34 +1000

    Add map company interior (#219)
    Fixes #211

---
+++ src/scenes/company_interior.js
@@ -0,0 +1,24 @@
+import gameObjects from '../gameObjects/map_company_interior';
+import { initMap } from '../init/map.init';
+import interactions from '../interactions/map_company_interior';
+import { k } from '../kplayCtx';
+
+export async function companyInterior() {
+    const objectConfig = {
+        static: [
+            'map_boundaries',
+            'building_boundaries',
+            'objects_enter_new_map',
+        ],
+        spawnpoints: ['spawnpoints'],
+        interactionObjects: ['interactions_objects'],
+    };
+    const [map, spawnpoint] = await initMap(
+        objectConfig,
+        './exports/maps/map_company_interior.png',
+        './maps/map_company_interior.json',
+        k.vec2(0, 11)
+    );
+
+    return [map, spawnpoint, gameObjects, interactions];
+}
