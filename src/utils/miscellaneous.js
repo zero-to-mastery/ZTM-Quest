f0618e0037eec7cd6ca267174ca1e13e9c72d990
Sri Harsha D V <dvsriharsha2198@gmail.com>
2025-10-13 12:32:53 +0530

    feat: move stats button to left panel

--- src/utils/miscellaneous.js
+++ src/utils/miscellaneous.js
@@ -1,4 +1,5 @@
-import { k } from '../kplayCtx';
+import { k, time } from '../kplayCtx';
+import { getGameState } from './gameState';
 
 // Mobile menu related
 const leftPanel = document.getElementById('left-panel');
@@ -48,3 +49,43 @@ const helpButton = document.getElementById('help-button');
 helpButton.addEventListener('click', showHelp);
 const helpCloseButton = document.getElementById('help-close-button');
 helpCloseButton.addEventListener('click', hideHelp);
+
+// Stats related
+
+const showStats = () => {
+    time.pause();
+    const player = getGameState().player;
+    const coinsCollected = player.coinsCollected;
+    const coinsSpent = player.coinsSpent;
+
+    // Calculation for time spent in game
+    const timeSpent = k.time();
+    const timeSpentSeconds = Math.floor(timeSpent % 60);
+    const timeSpentMinutes = Math.floor((timeSpent / 60) % 60);
+    const timeSpentHours = Math.floor(timeSpent / 3600);
+    const timeSpentString = `${timeSpentHours}h ${timeSpentMinutes}m ${timeSpentSeconds}s`;
+
+    // Adding updated info in html
+    const coinsCollectedElement = document.getElementById('coins-collected');
+    coinsCollectedElement.innerHTML = `Coins collected : ${coinsCollected}`;
+    const coinsSpentElement = document.getElementById('coins-spent');
+    coinsSpentElement.innerHTML = `Coins spent : ${coinsSpent}`;
+    const timeSpentElement = document.getElementById('time-spent');
+    timeSpentElement.innerHTML = `Time spent : ${timeSpentString}`;
+
+    showAlertWindow();
+    const statsAlert = document.querySelector('.stats-alert');
+    statsAlert.classList.add('display-block');
+};
+
+const hideStats = () => {
+    time.unpause();
+    const statsAlert = document.querySelector('.stats-alert');
+    statsAlert.classList.remove('display-block');
+    hideAlertWindow();
+};
+
+const statsButton = document.getElementById('stats-button');
+statsButton.addEventListener('click', showStats);
+const statsCloseButton = document.getElementById('stats-close-button');
+statsCloseButton.addEventListener('click', hideStats);
