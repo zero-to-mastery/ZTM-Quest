f1b77cd9551f35fea268325d6076968602daaf9c
blazenn2 <hamzanawab.bs@gmail.com>
2025-10-13 14:02:36 +0500

    Fix the wrong name of npc

--- src/interactions/map_arcade/index.js
+++ src/interactions/map_arcade/index.js
@@ -6,7 +6,7 @@ import { interactionWithGameMachine2 } from './game_machine_2.interactions';
 import { interactionWithGameMachine4 } from './game_machine_4.interactions';
 import { interactionWithGameMachine6 } from './game_machine_6.interactions';
 import { interactionWithJessie } from './jessie.interaction';
-import { interactionWithRico } from './lindsey.interaction';
+import { interactionWithRico } from './rico.interaction';
 import { interactionWithGameMachine10 } from './game_machine_10.interactions';
 import { interactionWithGameMachine11 } from './game_machine_11.interaction';
 import { interactionWithGameMachine12 } from './game_machine_12.interaction';
